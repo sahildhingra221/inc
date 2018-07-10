8e2b36ff0d112e5e1ebbf061e945c1fb3c86c7eb
sahildhingra221 <sahildhingra221@gmail.com>
2018-07-11 03:43:40 +0530

    --
    --

---
+++ js/incrementing.js
@@ -0,0 +1,25 @@
+$(function() {
+
+  $(".tm-quantity").append('<div class="inc button-incdc">+</div><div class="dec button-incdc">-</div>');
+
+  $(".button-incdc").on("click", function() {
+
+    var $button = $(this);
+    var oldValue = $button.parent().find("input").val();
+
+    if ($button.text() == "+") {
+  	  var newVal = parseFloat(oldValue) + 1;
+  	} else {
+	   // Don't allow decrementing below zero
+      if (oldValue > 0) {
+        var newVal = parseFloat(oldValue) - 1;
+	    } else {
+        newVal = 0;
+      }
+	  }
+
+    $button.parent().find("input").val(newVal);
+
+  });
+
+});
